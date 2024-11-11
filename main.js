3421bb15bb8f8105c270e68faa352ea5d6fa0c33
Evgenii <mazzabrazzer@gmail.com>
2024-11-11 12:58:33 +0400

    -

--- main.js
+++ main.js
@@ -253,13 +253,13 @@ document.getElementById('apply-makeup-look-1').onclick = async function() {
   // deepAR.switchEffect('./effects/look1');
   if (src_myBeauty) {
     src_myBeauty.faceMorphing.eyeSize.set(-50);
-    // myBeauty.skinSmoothing.set(85);
-    // myBeauty.faceMakeup.blush.intensity.set(40);
-    // myBeauty.faceMakeup.blush.color.set({r:226, g:132, b:130, a:255});
-    src_myBeauty.lipMakeup.lipstick.enable.set(true);
-    src_myBeauty.lipMakeup.lipstick.shade.setTemplate("matteNude");
-    src_myBeauty.lipMakeup.lipstick.amount.set(70);
-    // myBeauty.colorFilters.filter.setTemplate("filmContrast");
+    src_myBeauty.skinSmoothing.set(85);
+    src_myBeauty.faceMakeup.blush.intensity.set(40);
+    src_myBeauty.faceMakeup.blush.color.set({r:226, g:132, b:130, a:255});
+    // myBeauty.lipMakeup.lipstick.enable.set(true);
+    // myBeauty.lipMakeup.lipstick.shade.setTemplate("matteNude");
+    // myBeauty.lipMakeup.lipstick.amount.set(70);
+    src_myBeauty.colorFilters.filter.setTemplate("filmContrast");
   } else {
     src_log('myBeauty effect null...', 'info');
   }
